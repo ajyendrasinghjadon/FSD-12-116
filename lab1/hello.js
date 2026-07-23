4aaca034cd31ef18f5bbbb6d87d2a4027e3e0fdf
ajyendrasinghjadon <ajyendrasinghjadonnn@gmail.com>
2026-07-23 11:53:56 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,18 @@
+console.log("Ajyendra Singh Jadon")
+
+//write a function to take two numbers as input and return their sum
+function sum(a, b) {
+    return a + b;
+}
+//or
+const add = (a,b) => {
+    return a + b;
+}
+//or
+const add2= (a,b) => a + b;
+
+console.log(sum(5, 10));
+console.log(add(3, 7));
+console.log(add2(2, 4));
+
+
